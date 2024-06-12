c652499a08afa88957a0e921a5564f7d076b10d8
Pedropaulosousilva <pedro.psousilva@gmail.com>
2024-06-12 10:20:59 -0300

    Tuplas_Dic.py

---
+++ Tuplas_Dic.py
@@ -0,0 +1,22 @@
+import time
+
+#tupla = tuple()
+
+tupla = (1)
+
+tupla = (1,)
+
+tupla = 1, 2, 3
+
+print(tupla)
+
+print(tupla[1])
+
+#tupla[0] = 100 # Erro ,pois não é possivel alterar uma tupla
+
+#Manipulando dicionarios:
+dic = {"semMundial:Palmeiras", "1mundial:Corinthians", "2mundias:SãoPaulo"}
+
+print(dic[0])
+
+time.sleep(3)
